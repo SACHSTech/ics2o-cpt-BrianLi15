1b1ab163bfcc3990baeeb530b0a1c379bbc31a58
Brian Li <brian.li23@ycdsbk12.ca>
2021-01-14 13:58:33 +0000

    First commit

---
+++ Examination.py
@@ -0,0 +1,22 @@
+# I will be importing all functions I will need to use for the examination
+import FunctionsClassUsed
+import datetime
+import time
+
+
+print("Welcome to the ICS2O1a Examination!")
+print("---------------------------------------------------------------------------------------------------------------"
+      "----")
+print(" This examination will cover every single subject learned in this quadmester, from the understanding computer\n "
+      "unit to the basics of python and even computer and society knowledge questions. All answers can be answered\n "
+      "with capital or lowercase letters, however, if you have a spelling error, it will not count towards your final\n "
+      "score. Follow the instruction on each section EXACTLY in order to ensure full marks when doing the\n "
+      "examination. Some questions will be worth more points than others. At the end of the examination,\n "
+      "the questions you got wrong will be displayed and the correct answer will be shown with it. There is going to\n "
+      "be a timer on this test, you will have 20 minutes to finish this test, if you do not finish the exam in time,\n "
+      "the exam will automatically close and the remianing questions unanswered will counted as zero. ")
+print("---------------------------------------------------------------------------------------------------------------"
+      "-----")
+
+FunctionsClassUsed.getinformation()
+FunctionsClassUsed.timer(int(1200))
---
+++ FunctionsClassUsed.py
@@ -0,0 +1,40 @@
+import time
+import datetime
+
+
+# This function asks the user for thier information
+def getinformation():
+    name = str(input("Write your name: "))
+    while True:
+        try:
+            grade = int(input("Write your current grade: "))
+            break
+        except ValueError:
+            print("Please input a number (No decimal or letters)")
+
+    teacher = str(input("Write your teacher's name: "))
+
+    current_date = datetime.datetime.now()
+    print("The current date and time is: " + str(current_date))
+    print("Once the exam starts, the timer will begin and you will have 20 minutes to do the exam. The timer will be "
+          "displayed for you to see.")
+    start_exam = input("Press enter to start the timer and exam: ")
+
+
+# This sets the timer, it will start when the user finishes the previous function
+def timer(t):
+   while t = True:
+        mins, secs = divmod(t, 60)
+        countdown = '{:02d}:{:02d}'.format(mins, secs)
+        print(countdown, end="\r")
+        time.sleep(1)
+        t -= 1
+    print("Exam is over")
+
+
+timer(int(1200))
+
+def questions():
+    number = int(input(""))
+    string = input("")
+
